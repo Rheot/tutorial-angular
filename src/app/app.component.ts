33c89df4fbbec8339fa73b1742096e137bbfbb62
Rheo Tan <rheo.tan@gmail.com>
2018-12-14 19:33:12 +0000

    displaying a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = "Rheo's Angular";
-  copyright = "Rheo 2018";
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Nasty Man' },
+  { id: 13, name: 'Bombastico' },
+  { id: 14, name: 'Celebritas' },
+  { id: 15, name: 'Magnetee' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Codeman' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Rheo' },
+  { id: 20, name: 'Tornz' }
+];
